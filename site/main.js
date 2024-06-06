e4e73b883e865fcc6a5938b0a688f8460f11d1ee
Marcus Roberto <marcusroberto1703@gmail.com>
2024-06-06 15:28:37 -0300

    Calculo - 1

--- site/main.js
+++ site/main.js
@@ -1,44 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
 
-    var salarioInput = document.getElementById('salario');
-    var opcoesSelect = document.getElementById('opcoes');
-    var calcularBtn = document.querySelector('.button-container input[type="submit"]');
-    var cancelarBtn = document.querySelector('.button-container input[type="reset"]');
-    var resultadoDiv = document.getElementById('resultado');
-
-    function calcularAliquota() {
-        var salario = parseFloat(salarioInput.value.replace(/\D/g, ''));
-        var faixaSelecionada = opcoesSelect.value.split('-');
-        var faixaLimite = parseFloat(faixaSelecionada[0].replace(/\D/g, ''));
-        var aliquota = parseFloat(faixaSelecionada[1].replace(/\D/g, '')) / 100;
-
-        var valorAliquota = salario <= faixaLimite ? salario * aliquota : faixaLimite * aliquota;
-
-        resultadoDiv.textContent = 'Valor da alíquota: R$ ' + valorAliquota.toLocaleString('pt-BR', { minimumFractionDigits: 2 });
-    }
-
-    function limparResultado() {
-        resultadoDiv.textContent = ''; // Limpa o conteúdo do elemento
-    }
-
-    calcularBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-        calcularAliquota();
-    });
-
-    cancelarBtn.addEventListener('click', function (event) {
-        limparResultado();
-    });
-});
-
-// script.js
 document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Evita o envio do formulário
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Simulação de validação (usuário e senha hardcoded)
+    // Simulação de validação 
     if (username === 'usuario' && password === 'senha') {
         alert('Login bem-sucedido! Redirecionando...');
         window.location.href = '/site/pag_inicial.html'; // Redireciona para a página após o login
@@ -46,3 +13,48 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
         alert('Usuário ou senha incorretos. Tente novamente.');
     }
 });
+
+
+function calcularRestituicao() {
+    var tipoTrabalho = document.getElementById('tipoTrabalho').value;
+    var salario = document.getElementById('salario').value;
+    var mesesTrabalhados = document.getElementById('mesesTrabalhados').value;
+
+    var aliquota;
+    if (tipoTrabalho === 'empregado') {
+        if (salario <= 1412.00) {
+            aliquota = 0.075;
+        } else if (salario <= 2666.68) {
+            aliquota = 0.09;
+        } else if (salario <= 4000.03) {
+            aliquota = 0.12;
+        } else if (salario <= 7786.02) {
+            aliquota = 0.14;
+        }
+    } else if (tipoTrabalho === 'facultativo') {
+        aliquota = 0.05;
+    } else if (tipoTrabalho === 'simplificado') {
+        aliquota = 0.11;
+    } else if (tipoTrabalho === 'mei-tac') {
+        aliquota = 0.12;
+    } else if (tipoTrabalho === 'individual') {
+        if (salario <= 7786.02) {
+            aliquota = 0.20;
+        }
+    }
+
+    var restituicao = salario * aliquota * mesesTrabalhados;
+
+    document.getElementById('resultado').innerText = 'O valor contribuído ao INSS é: R$ ' + restituicao.toFixed(2);
+
+}
+
+function limparCampos() {
+    document.getElementById('tipoTrabalho').value = '';
+    document.getElementById('salario').value = '';
+    document.getElementById('mesesTrabalhados').value = '';
+    document.getElementById('resultado').innerText = '';
+}
+
+
+
